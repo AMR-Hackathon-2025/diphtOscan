c4d342054651d375432b64c4afa4407eb2388198
Martin RETHORET-PASTY <martin.rethoret-pasty@pasteur.fr>
2023-10-09 17:08:40 +0200

    Removing old tox database when updating

--- module/updating_database.py
+++ module/updating_database.py
@@ -3,14 +3,15 @@ import os
 
 from module.download_alleles_st import create_db, download_profiles_st, download_profiles_tox
 
-def update_database(arguments):
+def update_database(arguments, mlst_database:tuple, tox_database:tuple):
     if arguments.update : 
-        os.system("rm "+ MLST_db[1] + "* " + MLST_db[2] + "* ")                  
+        os.system("rm "+ mlst_database[1] + "* " + mlst_database[2] + "* ")                  
         print("Downloading MLST database")
         path_mlst_sequences, loci_mlst = create_db("pubmlst_diphtheria_seqdef", "3", arguments.path +"/data/mlst")
         download_profiles_st ("pubmlst_diphtheria_seqdef", "3", arguments.path +"/data/mlst", loci_mlst)
         print("   ... done \n")
-        
+
+        os.system("rm "+ tox_database[1] + "* " + tox_database[2] + "* ")                  
         print("Downloading tox database")
         path_tox_sequences, loci_tox = create_db("pubmlst_diphtheria_seqdef", "4", arguments.path +"/data/tox")
         download_profiles_tox ("pubmlst_diphtheria_seqdef", "4", arguments.path +"/data/tox")
