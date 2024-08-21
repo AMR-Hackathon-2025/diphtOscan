559cd7a6eefe072aa190aa63b670813e49307131
mrethore <martin.rethoret-pasty@pasteur.fr>
2024-08-21 12:09:44 +0200

    Splitting update_database to allow update of missing Class & Subclass in fam.tab

--- data/resistance/update_database_resistance.sh
+++ data/resistance/update_database_resistance.sh
@@ -19,23 +19,3 @@ cat $PATH_DB/Corynebacterium_diphtheriae/AMRProt_Cd >> $PATH_DB/$DATE/AMRProt
 sed '1d' $PATH_DB/Corynebacterium_diphtheriae/AMRProt-mutation_Cd.tab >> $PATH_DB/$DATE/AMRProt-mutation.tab
 #sed '1d' $PATH_DB/Corynebacterium_diphtheriae/AMRProt-susceptible_Cd.tab >> $PATH_DB/$DATE/AMRProt-susceptible.tab
 sed '1d' $PATH_DB/Corynebacterium_diphtheriae/fam_Cd.tab >> $PATH_DB/$DATE/fam.tab
-
-echo "Indexing" ;
-
-hmmpress -f $PATH_DB/$DATE/AMR.LIB > /dev/null 2> /dev/null
-
-makeblastdb -in $PATH_DB/$DATE/AMRProt -dbtype prot  -logfile /dev/null
-makeblastdb -in $PATH_DB/$DATE/AMR_CDS -dbtype nucl  -logfile /dev/null
-
-taxgroups=$(awk '{if ($3>0 && $1!="#taxgroup") print $1}' $PATH_DB/$DATE/taxgroup.tab)
-for taxgroup in $taxgroups  
-do makeblastdb -in $PATH_DB/$DATE/AMR_DNA-$taxgroup -dbtype nucl  -logfile /dev/null
-done
-
-
-echo -e "Corynebacterium_diphtheriae\tCorynebacterium_diphtheriae\t0" >> $PATH_DB/$DATE/taxgroup.tab
-
-PATH_DB="$PATH_DB/$DATE" 
-VERSION="$DATE"
-echo "Database directory: '$PATH_DB'"
-echo "Database version: $DATE.1"
--- module/updating_database.py
+++ module/updating_database.py
@@ -1,10 +1,46 @@
-import sys
+import datetime
 import os
+import sys
+
+import pandas as pd
 
 from module.download_alleles_st import create_db, download_profiles_st, download_profiles_tox
 
+node_class = {'pld':'OTHER_TOXINS',
+'spaA' : 'SpaA-type_pili_diphtheriae',
+'spaB' : 'SpaA-type_pili_diphtheriae',
+'spaC' : 'SpaA-type_pili_diphtheriae',
+'srtA' : 'SpaA-type_pili_diphtheriae',
+'spaD' : 'SpaD-type_pili_diphtheriae',
+'spaE' : 'SpaD-type_pili_diphtheriae',
+'spaF' : 'SpaD-type_pili_diphtheriae',
+'srtB' : 'SpaD-type_pili_diphtheriae',
+'srtC' : 'SpaD-type_pili_diphtheriae',
+'spaG' : 'SpaH-type_pili_diphtheriae',
+'spaH' : 'SpaH-type_pili_diphtheriae',
+'spaI' : 'SpaH-type_pili_diphtheriae',
+'srtD' : 'SpaH-type_pili_diphtheriae',
+'srtE' : 'SpaH-type_pili_diphtheriae',
+'tox' : 'TOXIN',
+'cbpA' : 'VIRULENCE/ADHESIN',
+'nanH' : 'VIRULENCE/ADHESIN',
+}
+
+def complete_missing_classification(path:str):
+    df = pd.read_csv(path, sep="\t", escapechar="\\", engine="python")
+    missing_class = df.loc[df['parent_node_id']=='VIRULENCE_Cdiphth']
+    for index in missing_class.index:
+        for field in ['class','subclass']:
+            if pd.isna(df.iloc[index, df.columns.get_loc(field)]) :
+                df.iloc[index, df.columns.get_loc(field)] = node_class[df.iloc[index]['#node_id']]
+    df.to_csv(path, sep="\t", escapechar="\\", index=False)
+    return
+
+
 def update_database(arguments, mlst_database:tuple, tox_database:tuple):
-    if arguments.update : 
+    if arguments.update :
+        date = datetime.datetime.today().strftime('%Y-%m-%d') 
+
         os.system("rm "+ mlst_database[1] + "* " + mlst_database[2] + "* ")                  
         print("Downloading MLST database")
         path_mlst_sequences, loci_mlst = create_db("pubmlst_diphtheria_seqdef", "3", arguments.path +"/data/mlst")
@@ -18,4 +54,6 @@ def update_database(arguments, mlst_database:tuple, tox_database:tuple):
         print("   ... done \n")
         
         os.system('bash ' + arguments.path + '/data/resistance/update_database_resistance.sh')
+        complete_missing_classification(arguments.path + '/data/resistance/' + date + '/fam.tab')
+        os.system('bash ' + arguments.path + '/data/resistance/making_blastdb.sh')
         print("   ... done \n\n\n")
